cb4a922b8bc58690544887176bd86b7066f05ac5
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2025-10-10 22:56:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump the version number when you deploy a new service worker
-const CACHE_NAME = 'jvp-spark-v1.0.0.dev.97';
+const CACHE_NAME = 'jvp-spark-v1.0.0.dev.98';
 const urlsToCache = [
   '/jvp-spark/',
   '/jvp-spark/index.html',
