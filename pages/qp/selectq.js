bf7e3c7afaafa19c744967057de778b169dc6757
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2026-01-06 12:28:32 +0530

    Log fetched data in selectq.js

--- pages/qp/selectq.js
+++ pages/qp/selectq.js
@@ -45,6 +45,7 @@ if (overallDifficulty === "random") {
          p_total_count: 9000,
          p_type_counts: {"MCQ": 1000, "True/False": 1000, "Fill up": 1000, "Very Short Answer Type": 1000, "Short Answer Type": 1000, "Long Answer Type": 1000, "Very Long Answer Type": 1000}
          }, false);
+         console.log(data);
          return data.map(convertQuestionToOldFormat) || [];
          } catch (err) {
          console.error("Error fetching questions:", err);
